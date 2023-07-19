ff2867bc009bcd4f113d3acd6a0f7ad10cc1cf1d
ngocnt123 <ngocnt8412@gmail.com>
2023-07-20 02:30:39 +0700

    commit bt4

---
+++ BT4.ts
@@ -0,0 +1,13 @@
+//S(n) = 1 + 2 + 3 + ... + n
+function TinhTong(n: number): number {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+      sum += i;
+    }
+    return sum;
+  }
+  
+  // Thử nghiệm hàm với một số n bất kì
+  const n: number = 5;
+  const ketQua: number = TinhTong(n);
+  console.log(`Tổng S(${n}) = ${ketQua}`);
