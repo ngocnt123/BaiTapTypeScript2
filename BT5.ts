51773a2351bab371838a8554796a3868dcdfa31c
ngocnt123 <139196981+ngocnt123@users.noreply.github.com>
2023-07-19 09:55:50 +0700

    Update BT5.ts

--- BT5.ts
+++ BT5.ts
@@ -1,8 +1,11 @@
-let arrayA = [5, 2, 6, 3, 8];
-let sum = 0, i = 0;
-while ( i < arrayA.length){
-    sum += arrayA[i];
-    ++i;
-}
+//let arrayA = [5, 2, 6, 3, 8];
+//let sum = 0, i = 0;
+//while ( i < arrayA.length){
+//    sum += arrayA[i];
+//    ++i;
+//}
+//console.log(sum); 
 
-console.log(sum); 
+let num = [5, 2, 6, 3, 8];
+let sum = num.reduce((sum, element) => sum + element, 0);
+console.log(sum);
