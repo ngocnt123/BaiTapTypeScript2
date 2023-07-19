994666246a3d2388bf14d3184e1f1c8c9278e970
hieutv2324 <139298697+hieutv2324@users.noreply.github.com>
2023-07-19 08:47:47 +0700

    Update baitap6.ts

--- baitap6.ts
+++ baitap6.ts
@@ -1,6 +1,6 @@
-  function divide({ John, Ann, Pete }: { John: number, Ann: number, Pete: number }) {
+  function add({ John, Ann, Pete }: { John: number, Ann: number, Pete: number }) {
     return John + Ann + Pete;
   }
   
-  console.log(divide({John: 100, Ann: 160, Pete: 130}));
+  console.log(add({John: 100, Ann: 160, Pete: 130}));
     
