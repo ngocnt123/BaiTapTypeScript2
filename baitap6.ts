8973489c50cc8951e6bdae97749b1346b6857d5b
hieutv2324 <tranvanhieu2324@gmail.com>
2023-07-19 08:25:13 +0700

    commit bt6

---
+++ baitap6.ts
@@ -0,0 +1,6 @@
+  function divide({ John, Ann, Pete }: { John: number, Ann: number, Pete: number }) {
+    return John + Ann + Pete;
+  }
+  
+  console.log(divide({John: 100, Ann: 160, Pete: 130}));
+    
