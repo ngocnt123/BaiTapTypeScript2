910583a7846f1c46ce41812b62ddcbd0e6a363ce
ngocnt123 <ngocnt8412@gmail.com>
2023-07-20 08:41:44 +0700

    commit bt3

---
+++ BT3.ts
@@ -0,0 +1,12 @@
+function laSoChan(n: number): boolean {
+    return n % 2 === 0;
+  }
+  
+  // Thử nghiệm hàm với một số n bất kỳ
+  const number: number = 10  ;
+  if (laSoChan(number)) {
+    console.log(`${number} là số chẵn.`);
+  } else {
+    console.log(`${number} không phải là số chẵn.`);
+  }
+
