4fcd3dd5e3eb7156fc77b67f1d698c01e0c99c2d
ngocnt123 <ngocnt8412@gmail.com>
2023-07-20 10:03:38 +0700

    fix bt3

--- BT3.ts
+++ BT3.ts
@@ -1,12 +1,12 @@
-function laSoChan(n: number): boolean {
-    return n % 2 === 0;
-  }
-  
-  // Thử nghiệm hàm với một số n bất kỳ
-  const number: number = 10  ;
-  if (laSoChan(number)) {
-    console.log(`${number} là số chẵn.`);
-  } else {
-    console.log(`${number} không phải là số chẵn.`);
-  }
+function isEvenNumber(n: number): boolean {
+  return n % 2 === 0;
+}
+
+// Thử nghiệm hàm với một số n bất kỳ
+const number: number = 10;
+if (isEvenNumber(number)) {
+  console.log(`${number} là số chẵn.`);
+} else {
+  console.log(`${number} không phải là số chẵn.`);
+}
 
