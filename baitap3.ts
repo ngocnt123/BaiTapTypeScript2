3ab5f45a21fcb532e26497576d8578357f8f54e0
hieutv2324 <tranvanhieu2324@gmail.com>
2023-07-24 11:06:46 +0700

    fix bt3

--- baitap3.ts
+++ baitap3.ts
@@ -1,13 +1,11 @@
 function laSoChan(n: number): boolean {
-    return n % 2 === 0;
-  }
-  
-  // Thử nghiệm hàm với một số n bất kỳ
-  const number: number = 9  ;
-  if (laSoChan(number)) {
-    console.log(`${number} là số chẵn.`);
-  } else {
-    console.log(`${number} không phải là số chẵn.`);
-  }
-  
-  
+  return n % 2 === 0;
+}
+
+// Thử nghiệm hàm với một số n bất kỳ
+const number: number = 9;
+if (laSoChan(number)) {
+  console.log(`${number} là số chẵn.`);
+} else {
+  console.log(`${number} không phải là số chẵn.`);
+}
