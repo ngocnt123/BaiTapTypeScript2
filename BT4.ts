d450f2dd2f12a8e896a63f83490ca8b54c7a2625
ngocnt123 <ngocnt8412@gmail.com>
2023-07-25 08:36:33 +0700

    fix bt4

--- BT4.ts
+++ BT4.ts
@@ -1,13 +1,13 @@
 //S(n) = 1 + 2 + 3 + ... + n
 function TinhTong(n: number): number {
-    let sum = 0;
-    for (let i = 1; i <= n; i++) {
-      sum += i;
-    }
-    return sum;
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
   }
-  
-  // Thử nghiệm hàm với một số n bất kì
-  const n: number = 5;
-  const ketQua: number = TinhTong(n);
-  console.log(`Tổng S(${n}) = ${ketQua}`);
+  return sum;
+}
+
+// Thử nghiệm hàm với một số n bất kì
+const n: number = 5;
+const ketQua: number = TinhTong(n);
+console.log(`Tổng S(${n}) = ${ketQua}`);
