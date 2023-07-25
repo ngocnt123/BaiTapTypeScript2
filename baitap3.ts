6cb0ec34647139dd0f3c9a332d38e873972f0985
hieutv2324 <tranvanhieu2324@gmail.com>
2023-07-25 08:57:58 +0700

    fix bt3

--- baitap3.ts
+++ baitap3.ts
@@ -1,10 +1,10 @@
-function Even(n: number): boolean {
+function isEvenNumber(n: number): boolean {
   return n % 2 === 0;
 }
 
 // Thử nghiệm hàm với một số n bất kỳ
 const number: number = 9;
-if (Even(number)) {
+if (isEvenNumber(number)) {
   console.log(`${number} là số chẵn.`);
 } else {
   console.log(`${number} không phải là số chẵn.`);
