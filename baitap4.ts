3b18967be8b8dc3d3f7329558ca73c0cf6b4ed2d
hieutv2324 <tranvanhieu2324@gmail.com>
2023-07-25 08:44:26 +0700

    fix bt4

--- baitap4.ts
+++ baitap4.ts
@@ -1,14 +1,13 @@
 //S(n) = 1 + 2 + 3 + ... + n
 function tinhTong(n: number): number {
-    let sum = 0;
-    for (let i = 1; i <= n; i++) {
-      sum += i;
-    }
-    return sum;
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
   }
-  
-  // Thử nghiệm hàm với một số n bất kỳ
-  const n: number = 5;
-  const ketQua: number = tinhTong(n);
-  console.log(`Tổng S(${n}) = ${ketQua}`);
-  
+  return sum;
+}
+
+// Thử nghiệm hàm với một số n bất kỳ
+const n: number = 5;
+const ketQua: number = tinhTong(n);
+console.log(`Tổng S(${n}) = ${ketQua}`);
