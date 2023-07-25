6840cad987be64ab75453666f24028b3124f0a7b
ngocnt123 <ngocnt8412@gmail.com>
2023-07-25 08:40:11 +0700

    fix bt6

--- BT6.ts
+++ BT6.ts
@@ -1,4 +1,4 @@
-function add({John, Ann, Pete }:{John: number, Ann: number, Pete: number}){
-    return John + Ann + Pete;
+function add({ John, Ann, Pete }: { John: number; Ann: number; Pete: number }) {
+  return John + Ann + Pete;
 }
-console.log(add({John: 100, Ann:160, Pete: 130}));
+console.log(add({ John: 100, Ann: 160, Pete: 130 }));
