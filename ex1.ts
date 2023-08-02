bbfc8b083707d9a015d036c72e3ddd93dcd5ccfc
hieutv2324 <tranvanhieu2324@gmail.com>
2023-08-02 08:51:42 +0700

    fix conflict

--- ex1.ts
+++
@@ -1,5 +0,0 @@
-function printCoord(pt: { x: number; y: number }) {
-  console.log("Tọa độ của x là " + pt.x);
-  console.log("Tọa độ của y là " + pt.y);
-}
-printCoord({ x: 5, y: 15 });
--- ex3.ts
+++
@@ -1,8 +0,0 @@
-let x: number = 90,
-  y = 75;
-
-if (x < y) {
-  console.log("x lớn hơn y.");
-} else {
-  console.log("x nhỏ hơn hoặc bằng y.");
-}
